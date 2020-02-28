6e5c2bd382dbfef52c109c187c357cc64d883e2e
TheClockTwister <wiesen.leon@gmail.com>
2020-02-28 18:03:47 +0100

    New version
    Simpole implementation, major reformats and methods

--- zappy.py
+++ zappy.py
@@ -1,97 +1,129 @@
-#!/usr/bin/env python3
 import time
-from pprint import pprint
 from zapv2 import ZAPv2
-import sys
 
-class Zappy:
-    def __init__(self, target):
-        self.target = target
-        self.apikey = 'changeme'
-        self.zap = ZAPv2(apikey=self.apikey)
-        
-    def access_target(self):
-        print('[+] Accessing target {}'.format(self.target))
-        self.zap.urlopen(self.target)
-        # Give the sites tree a chance to get updated
-        time.sleep(2)        
 
-    def spider_target(self):
-        print('[+] Spidering target {}'.format(self.target))
-        self.zap.spider.set_option_max_depth(400)
-        scanid = self.zap.spider.scan(self.target)
-        time.sleep(2)
-        
-        # Wait for Spider
-        while (int(self.zap.spider.status(scanid)) < 100):
-           print('| Spider progress %: {}'.format(self.zap.spider.status(scanid)))
-           time.sleep(2)
-        print ('[+] Spider completed')
+class ScanPluginObject:
+    def __init__(self, attack_name, unknown, status, progress, time_elapsed, requests, alerts):
+        self.attack_name = attack_name
+        self.unknown = unknown
+        self.status = status
+        self.progress = progress
+        self.time_elapsed = time_elapsed
+        self.requests = requests
+        self.alerts = alerts
 
-    def active_scan_target(self):
-        print ('[+] Active Scanning target {}'.format(self.target))
-        scanid = self.zap.ascan.scan(self.target)
-        while (int(self.zap.ascan.status(scanid)) < 100):
+
+class ScanAlert:
+    def __init__(self, sourceid, other, method, evidence, pluginId, cweid, confidence, wascid, description,
+                 messageId, url, reference, solution, alert, param, attack, name, risk, id):
+        self.sourceid = int(sourceid) if sourceid != "" else None
+        self.other = other
+        self.method = method
+        self.evidence = evidence
+        self.plugin_id = int(pluginId) if pluginId != "" else None
+        self.cweid = cweid
+        self.confidence = confidence
+        self.wascid = int(wascid) if wascid != "" else None
+        self.description = description
+        self.message_id = int(messageId) if messageId != "" else None
+        self.url = url
+        self.reference = reference
+        self.solution = solution
+        self.alert = alert
+        self.param = param
+        self.attack = attack
+        self.name = name
+        self.risk = risk
+        self.id = int(id)
+
+
+class Scan:
+    def __init__(self, target, api_key: str = None, host: str = "localhost", port: int = 8080):
+
+        self.target = target[:-1] if target.endswith("/") else target  # The URL of the application to be tested (and delete "/" at end)
+        self._zap = ZAPv2(apikey=api_key, proxies={'http': f'http://{host}:{port}', 'https': f'http://{host}:{port}'})  # Connect to API endpoint
+        self.urls = []
+
+    class Crawler:
+        def __init__(self, zap: ZAPv2, target: str):
+            self.zap = zap
+            self.scan_id = zap.spider.scan(target)
+
+        def progress(self) -> int:
+            return int(self.zap.spider.status(self.scan_id))
+
+        def results(self):
+            return list(map(str, self.zap.spider.results(self.scan_id)))
+
+    def start_crawler(self) -> Crawler:
+        return self.Crawler(self._zap, self.target)
+
+    class Attack:
+        def __init__(self, zap: ZAPv2, target: str):
+            self.zap = zap
+            self.target = target
+            self.scan_id = zap.ascan.scan(target)
+
+        def progress(self) -> int:
+            return int(self.zap.ascan.status(self.scan_id))
+
+        def results(self):
+            return [ScanAlert(**x) for x in self.zap.alert.alerts(baseurl=self.target)]
+
+    def start_attack(self) -> Attack:
+        return self.Attack(self._zap, self.target)
+
+    def run_full_scan(self):
+        scan_id = self._zap.ascan.scan(self.target)
+        while int(self._zap.ascan.status(scan_id)) < 100:
             # Loop until the scanner has finished
-            print ('| Scan progress %: {}'.format(self.zap.ascan.status(scanid)))
-            time.sleep(60)
-        print ('[+] Active Scan completed')
+            print('Scan progress %: {}'.format(self._zap.ascan.status(scan_id)))
+
+            scan = self._zap.ascan.scan_progress(scan_id)  # get all (saved and active) self._zap scans
+
+            for i in range(len(scan)):  # search for scan on this self.target
+                if scan[i] == self.target:  # if self.target found
+                    scan_progress = scan[i + 1]
+                    scan_attacks = [ScanPluginObject(*scan["Plugin"]) for scan in scan_progress["HostProcess"]]
+                    time.sleep(5)
+                    break
+            else:  # if self.target not found in scan list
+                raise Exception("Scan not found in self._zap scan list.")
+
+        alerts = self._zap.alert.alerts(baseurl=self.target)
+
+        informational = [alert for alert in alerts if alert["risk"] == "Informational"]
+        low = [alert for alert in alerts if alert["risk"] == "Low"]
+        medium = [alert for alert in alerts if alert["risk"] == "Medium"]
+        high = [alert for alert in alerts if alert["risk"] == "High"]
+
+        print(f"""
+        Informational: {len(informational)}
+        Low          : {len(low)}
+        Medium       : {len(medium)}
+        High         : {len(high)}
+        """
+              )
 
 
-    def report_results(self):
-        st = 0
-        pg = 5000
-        alert_dict = {}
-        alert_count = 0
-        alerts = self.zap.core.alerts()
-        
-        if not alerts:
-            print("[-] Could not detect any vulnerabilities")
-        for alert in alerts:
-            with open("Zappy_{}.txt".format(""),"a+") as output:
-                plugin_id = alert.get('pluginId')
-                if alert.get('risk') == 'High':
-                    print("""
-                 
-Vulnerability: {}
-Risk: {}
-URL: {}
-                 
-                 """.format(alert.get('name'),alert.get('risk'),alert.get('url')))
-                    output.write("""
-                     
-Vulnerability: {}
-Risk: {}
-URL: {}
-                     
-                     """.format(alert.get('name'),alert.get('risk'),alert.get('url')))
+if __name__ == '__main__':
+    scan = Scan(target=input("target URL: "))
 
+    crawler = scan.start_crawler()
+    print("started crawler.")
+    while crawler.progress() < 100:
+        print(crawler.progress())
+    print(crawler.results())
 
+    attack = scan.start_attack()
+    while attack.progress() < 100:
+        print(attack.progress())
+    alerts = attack.results()
 
-    def run(self):
-        self.access_target()
-        self.spider_target()
-        self.active_scan_target()
-        self.report_results()
-        
-if __name__ == "__main__":
-    if len(sys.argv) < 2:
-        print("[-] Don't forget to add a target list!")
-        print("[-] Usage: python3 zappy.py target_list.txt")
-        sys.exit()
-    with open(sys.argv[1]) as target_list:
-        for target_url in target_list:
-            try:
-                target_url = target_url.rstrip()
-                print("""
-=======================================================================================
+    informational = [alert for alert in alerts if alert.risk == "Informational"]
+    low = [alert for alert in alerts if alert.risk == "Low"]
+    medium = [alert for alert in alerts if alert.risk == "Medium"]
+    high = [alert for alert in alerts if alert.risk == "High"]
 
-Initiating Scan for target: {}
-
-=======================================================================================        
-                
-                """.format(target_url))
-                scanner = Zappy(target_url)
-                scanner.run()
-            except:
-                pass
+    print("High risk alerts:")
+    [print(alert, "\n") for alert in high]
